e5c36a5e2748f6f58118a7331998b5753a6380ff
Hitman <38419085+hitman-045@users.noreply.github.com>
2021-08-22 18:03:44 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,5 +2,5 @@ cd ~
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sleep 2
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sleep 5
+sleep 2
 yum install jenkins -y
