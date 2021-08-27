a4e6f3a37c684717586118e6359d1bccafe23ce6
Hitman <38419085+hitman-045@users.noreply.github.com>
2021-08-27 13:04:06 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~
+sudo amazon-linux-extras install epel -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sleep 2
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
